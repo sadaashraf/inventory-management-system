ae073d3413cc0b20ce3f936bc00272203fe48a1c
sadaashraf <nayeemashraf92@gmail.com>
2024-04-01 23:01:32 +0500

    register connect backend

---
+++ Backend/Controller/auth.js
@@ -0,0 +1,76 @@
+import User from "../Models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+//================================||
+//          REGISTER              ||
+//================================||
+const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
+
+    await newUser.save();
+    res.status(200).json({
+      success: true,
+      message: "User has been Registered successfully.",
+    });
+    
+  } catch (err) {
+    next(err);
+  }
+};
+
+//================================||
+//            LOGIN               ||
+//================================||
+const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Username and password are required.",
+      });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Username or password is incorrect.",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        success: false,
+        message: "Username or password is incorrect.",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    const { password: userPassword, ...otherDetails } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ details: { ...otherDetails } });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export { register, login };
